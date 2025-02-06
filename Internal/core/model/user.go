fb064751ff47d7e25355eb4f705250abfce88dee
Barlus Cuda <barluscuda@gmail.com>
2025-02-06 16:43:12 +0700

    fix model

--- Internal/core/model/user.go
+++ Internal/core/model/user.go
@@ -8,7 +8,7 @@ type User struct {
 	ID       uint   `gorm:"primaryKey"`
 	Name     string `gorm:"size:255;not null"`
 	Nickname string `gorm:"size:255"`
-	Bio      string `gorm:"type:text; default:bo bio yet"`
+	Bio      string `gorm:"type:text; default:no bio yet"`
 
 	Username  string	`gorm:"size:255;unique;not null"`
 	Email     string    `gorm:"size:255;unique;not null"`
