ba476c0d55ffe75de6b6bc53b100f2485d66eeee
Barlus Cuda <barluscuda@gmail.com>
2025-02-07 20:53:34 +0700

    add json for user model

--- Internal/core/model/user.go
+++ Internal/core/model/user.go
@@ -5,16 +5,16 @@ import (
 )
 
 type User struct {
-	ID       uint   `gorm:"primaryKey"`
-	Name     string `gorm:"size:255;not null"`
-	Nickname string `gorm:"size:255"`
-	Bio      string `gorm:"type:text; default:no bio yet"`
+	ID       uint   `gorm:"primaryKey" json:"id"`
+	Name     string `gorm:"size:255;not null" json:"name"`
+	Nickname string `gorm:"size:255" json:"nickname"`
+	Bio      string `gorm:"type:text; default:no bio yet" json:"bio"`
 
-	Username  string	`gorm:"size:255;unique;not null"`
-	Email     string    `gorm:"size:255;unique;not null"`
-	Password  string    `gorm:"size:255;not null"`
-	Role      string    `gorm:"size:255;not null;default:user"`
+	Username string `gorm:"size:255;unique;not null" json:"username"`
+	Email    string `gorm:"size:255;unique;not null" json:"email"`
+	Password string `gorm:"size:255;not null" json:"password"`
+	Role     string `gorm:"size:255;not null;default:user" json:"role"`
 
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"create_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"update_at"`
 }
