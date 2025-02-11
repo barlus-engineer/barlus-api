a9a3408d998d757a3dcd49d2af9f99dc07f20adb
Barlus Cuda <barluscuda@gmail.com>
2025-02-11 21:22:40 +0700

    add hash password check

--- pkg/text/hashPassword.go
+++ pkg/text/hashPassword.go
@@ -1,14 +1,19 @@
 package text
 
 import (
+	"github.com/barlus-engineer/barlus-api/pkg/logger"
 	"golang.org/x/crypto/bcrypt"
-	"log"
 )
 
 func HashPassword(password string) string {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		log.Fatal(err)
+		logger.Crash("hashpassword: ", err)
 	}
 	return string(hashedPassword)
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
